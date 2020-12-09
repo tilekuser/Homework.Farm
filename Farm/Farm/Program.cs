8a6011dac2a70114ef197ea38b124e1b985ef50b
tilekuser <tilek.niyazbekov@mail.ru>
2020-12-09 16:15:39 +0600

    hih

--- Farm/Farm/Program.cs
+++ Farm/Farm/Program.cs
@@ -10,7 +10,7 @@ namespace Farm
             var s = new Farmm("Happy farm", "Bishkek city, street Ankara 2");
             s.AddAny();
             s.AddCow();
-            s.AddHorse();
+           
    
         }
     }
